99c9c68346ff4b9aba725be0db6b52b5c60d2e3c
RolaHElsaeed <85562694+RolaHElsaeed@users.noreply.github.com>
2021-06-08 15:03:27 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var saveduserimput = prompt("hello! how was your day");
+
+console.log(saveduserimput);
+
+document.getElementById("h").innerText = saveduserimput;
+
+//tooltip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
